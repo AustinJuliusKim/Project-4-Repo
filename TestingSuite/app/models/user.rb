c4457f0f51b95545bade27559d533bebd0215146
Mike Pacheco <mikelnpacheco@gmail.com>
2015-05-27 22:59:43 -0700

    model testing

--- TestingSuite/app/models/user.rb
+++ TestingSuite/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  validates :firstname, :lastname, :companyname, :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false }
+
+
+  def full_name
+  "#{self.firstname} #{self.lastname}"
+end
+
 end
