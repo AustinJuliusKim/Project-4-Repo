1e9126f416f7c609c726db064151eddb1cce6ef5
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-05-28 21:11:40 -0700

    Jimmy black voodoo magic

--- TestingSuite/app/models/order.rb
+++ TestingSuite/app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+
+  # def details(options={})
+  # 	@egg_size
+  # 	@egg_color
+  # 	@quantity
+  # end
+
+  
 end
--- TestingSuite/config/routes.rb
+++ TestingSuite/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :users do
   		resources :orders
   	end
+
+  	resources :orders #, only: [:new, :index, :create, :show]
 end
