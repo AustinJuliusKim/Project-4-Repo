c8e8ca49fc33eaa7a073655222524334971a010e
Austin Julius Kim <austinjuliuskim@gmail.com>
2015-06-04 22:44:23 -0700

    fixes

--- TheGoodEgg/app/controllers/users_controller.rb
+++ TheGoodEgg/app/controllers/users_controller.rb
@@ -10,8 +10,9 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    @user = current_user || User.find(params[:id])
     @orders = @user.orders.where(user_id: @user.id)
+
   end
 
   # GET /users/new
@@ -73,10 +74,6 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :password, :name)
-    end
-
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
 
--- TheGoodEgg/app/models/user.rb
+++ TheGoodEgg/app/models/user.rb
@@ -6,9 +6,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         private
-
-        	def current_user
-
-        	end
+        
 end
